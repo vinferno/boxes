f03d773bcf1d91eb8a8df875592c97416cec044d
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2019-08-01 22:38:06 -0700

    test - test server

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express();
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 3000;
+    port = 5000;
 }
-app.listen(port);
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

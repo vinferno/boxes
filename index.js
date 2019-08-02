7ac21a91cb01b4941118235958173b3438f515e1
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2019-08-01 22:31:39 -0700

    test - test server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('works');
+const express = require('express')
+const app = express();
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port);
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))

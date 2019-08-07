ac5bf0defa22d6ff6795f8f0523e935422d88de1
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2019-08-07 07:15:19 -0700

    update upload

--- boxes/src/app/features/file-uploader/file-uploader.component.ts
+++ boxes/src/app/features/file-uploader/file-uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ImagesService} from "../../services/images.service";
 
 @Component({
   selector: 'vf-file-uploader',
@@ -6,23 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-uploader.component.scss']
 })
 export class FileUploaderComponent implements OnInit {
-  uploadedFiles;
-  constructor() { }
+  public uploadedFiles;
+  public imageSrc;
+  constructor(private imagesService: ImagesService) { }
 
   ngOnInit() {
+    this.imagesService.getAll().subscribe( res => {
+      console.log('res', res[0].image.data.data);
+      this.imageSrc = this.getSrcFromBuffer(res[0].image.data.data)
+    });
   }
   fileChange(element) {
     this.uploadedFiles = element.target.files;
+    console.log(this.uploadedFiles);
+
   }
 
   upload() {
     const formData = new FormData();
-    for (var i = 0; i < this.uploadedFiles.length; i++) {
-      formData.append("uploads[]", this.uploadedFiles[i], this.uploadedFiles[i].name);
+    for (let i = 0; i < this.uploadedFiles.length; i++) {
+      formData.append("upload", this.uploadedFiles[i], this.uploadedFiles[i].name);
     }
-    this.http.post('/api/upload', formData)
-      .subscribe((response) => {
+    this.imagesService.upload(formData).subscribe((response) => {
         console.log('response received is ', response);
-      })
+      });
+  }
+  public readURL(event: any): void {
+
+    if (event.target.files && event.target.files[0]) {
+      this.uploadedFiles = event.target.files;
+      console.log(this.uploadedFiles);
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.imageSrc = reader.result;
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+
+  getSrcFromBuffer(buf) {
+    console.log('getSrcs')
+    const arrayBufferView = new Uint8Array( buf );
+    const blob = new Blob( [ arrayBufferView ], { type: "image/png" } );
+    const urlCreator = window.URL;
+    this.imageSrc = urlCreator.createObjectURL( blob );
   }
 }
---
+++ boxes/src/app/services/images.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {ApiService} from "./api.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagesService {
+
+  constructor(private apiService: ApiService) { }
+
+  public upload(formData) {
+    return this.apiService.post<Observable<any>>('upload', formData);
+  }
+  public getAll() {
+    return this.apiService.get<Observable<any>>('images');
+  }
+}
